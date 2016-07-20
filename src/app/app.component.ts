bb96ca5d4457b79c14cdc59a6be5bf4d625dfccc
Ovidiu Buligan <Ovidiu Buligan>
2016-07-20 21:12:56 +0300

    style the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,17 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  template: '<h1>My First Angular 2 App</h1>',
+  // styles can be declared inline 
+  styles: [`
+      h1 {
+        color: #369;
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 250%;
+      }
+      body {
+        margin: 2em;
+      }
+  `]
 })
 export class AppComponent { }
